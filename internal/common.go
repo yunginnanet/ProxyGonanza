a8cf7e064c7abe906817447c4aa09d5eb6670dd0
kayos@tcp.direct <kayos@tcp.direct>
2022-07-17 21:06:29 -0700

    Improve efficiency of existing methods and add new methods

--- internal/common.go
+++ internal/common.go
@@ -1,7 +1,8 @@
-package example
+package internal
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -16,7 +17,10 @@ func ParseArgs() {
 		fmt.Printf("\t  ~*~ ProxyGonanza ~*~ \nhttps://git.tcp.direct/kayos/proxygonanza\n\nFatal: missing API Key \n\nUsage: %s [--verbose|-v] '<apikey>'\n\n", os.Args[0])
 		os.Exit(1)
 	}
-	for _, arg := range os.Args {
+	for i, arg := range os.Args {
+		if i == 0 {
+			continue
+		}
 		switch arg {
 		case "-d", "--debug", "-v", "--verbose":
 			Debug = true
@@ -27,4 +31,13 @@ func ParseArgs() {
 		}
 	}
 
+}
+
+// CloseBody is crude error handling for any potential errors closing the response body.
+func CloseBody(res *http.Response) {
+	err := res.Body.Close()
+	if err != nil {
+		println("WARN: ProxyGonanza failed to close body for request to ",
+			res.Request.RequestURI+": "+err.Error())
+	}
 }
